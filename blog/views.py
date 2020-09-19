fbb467702c05b8ebbf7561bb1c5c56482dc5707f
Adesh Nalpet <390.adesh@gmail.com>
2020-09-19 20:53:56 +0530

    Add redis for database caching

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.conf import settings
+from django.views.decorators.cache import cache_page
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
+@cache_page(CACHE_TTL)
 def index(request):
     posts = Post.objects.order_by('-updated_at')[:5]
     context = {
@@ -10,6 +16,7 @@ def index(request):
     return render(request, 'index.html', context=context)
 
 
+@cache_page(CACHE_TTL)
 def post(request, post_id):
     post_detail = get_object_or_404(Post, post_id=post_id)
     context = {
