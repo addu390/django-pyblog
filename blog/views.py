a4ed5e9224ada7bd4b7a892c95e6a261809febfb
Adesh Nalpet <390.adesh@gmail.com>
2020-09-21 23:47:36 +0530

    Add cache page annotation

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 CATEGORIES = {'categories': [list(item) for item in Post.categories]}
 
 
-# @cache_page(CACHE_TTL)
+@cache_page(CACHE_TTL)
 def index(request):
     # [OFFSET:OFFSET+LIMIT]
     posts = Post.objects.order_by('-updated_at')[:5]
@@ -20,7 +20,7 @@ def index(request):
     return render(request, 'index.html', context=context)
 
 
-# @cache_page(CACHE_TTL)
+@cache_page(CACHE_TTL)
 def post(request, post_id):
     post_detail = get_object_or_404(Post, post_id=post_id)
     context = {
